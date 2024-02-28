a34e86cb14a7015d59cade5cb2d2b6364317622e
Shaddy <shadybett540@gmail.com>
2024-02-28 15:31:10 +0300

    made student dashboard

--- src/pages/DashboardStudent.js
+++ src/pages/DashboardStudent.js
@@ -2,31 +2,91 @@ import React from "react";
 import Nav from "../components/Nav";
 import { Link } from "react-router-dom";
 
-
 function DashboardStudent() {
   return (
     <div>
       <Nav />
-      <Link to="/students" className="block w-full max-w-[500px]">
-        <div className="flex flex-col gap-4 items-center px-4 pb-4 pt-14 rounded-lg bg-light-orange hover:bg-t-orange transition">
-          <h3 className="text-4xl ">Class name</h3>
-          <h4 className="text-2xl ">8:00 AM - 10:00AM</h4>
-          <h2 className="text-2xl font-medium">Attendance Recap</h2>
-          <div className="flex justify-between w-full">
-            <div className="text-lg">
-              Present: 6 | Absent: 5 | Remaining: 20
+      <h3 className="mb-4 mt-4 font-medium ml-10">Welcome student name</h3>
+      <div className="flex ml-10">
+        <Link to="/students" className="block max-w-[500px]">
+          <div className="mt-4 flex flex-col gap-1 px-4 pb-4 pt-14 rounded-lg bg-light-orange hover:bg-t-orange transition mb-4">
+            <div className="flex justify-between items-center">
+              <h3 className="text-2xl font-medium mt-2">Class name</h3>
+              <div className="text-lg mt-4">
+                {" "}
+                <div>
+                  {" "}
+                  <span style={{ color: "orange", fontSize: "1.3em" }}>
+                    18
+                  </span>{" "}
+                  Days
+                </div>{" "}
+                Remaining
+              </div>
             </div>
-            <div className="text-lg">Attendance %: 50%</div>
+            <div className="mb-5">
+              <h4 className="text-2l ">8:00 AM - 10:00 AM</h4>
+              <div className="flex justify-between items-center">
+                <div className="text-lg mr-4">Attendance</div>
+                <div className="text-lg mr-4">Present</div>
+                <div className="text-lg mr-4">Absent</div>
+                <div className="text-lg mr-4">Late</div>
+              </div>
+              <div className="flex justify-between items-center">
+                <h4 className="text-2l font-medium mr-4">50%</h4>
+                <h4 className="text-2l font-medium ml-2">5</h4>
+                <h4 className="text-2l font-medium mr-2">10</h4>
+                <h4 className="text-2l font-medium mr-6">20</h4>
+              </div>
+            </div>
+            <button className="btn py-2 self-end mr-4 mt-4">Check in</button>
           </div>
-        </div>
-      </Link>
+        </Link>
 
-      <h3>Generate Report</h3>
-      <input type="text" placeholder="Select period" />
-      <button placeholder="Download"></button>
-      <button className="btn py-2">Download</button>
+        <Link to="/students" className="block max-w-[500px] ml-20">
+          <div className="mt-4 flex flex-col gap-1 px-4 pb-4 pt-14 rounded-lg bg-light-orange hover:bg-t-orange transition mb-4">
+            <div className="flex justify-between items-center">
+              <h3 className="text-2xl font-medium mt-2">Class name</h3>
+              <div className="text-lg mt-4">
+                {" "}
+                <div>
+                  {" "}
+                  <span style={{ color: "orange", fontSize: "1.3em" }}>
+                    18
+                  </span>{" "}
+                  Days
+                </div>{" "}
+                Remaining
+              </div>
+            </div>
+            <div className="mb-5">
+              <h4 className="text-2l ">8:00 AM - 10:00 AM</h4>
 
+              <div className="flex justify-between items-center">
+                <div className="text-lg mr-4">Attendance</div>
+                <div className="text-lg mr-4">Present</div>
+                <div className="text-lg mr-4">Absent</div>
+                <div className="text-lg mr-4">Late</div>
+              </div>
+              <div className="flex justify-between items-center">
+                <h4 className="text-2l font-medium mr-4">50%</h4>
+                <h4 className="text-2l font-medium ml-2">5</h4>
+                <h4 className="text-2l font-medium mr-2">10</h4>
+                <h4 className="text-2l font-medium mr-6">20</h4>
+              </div>
+            </div>
+            <button className="btn py-2 self-end mr-4 mt-4">Check in</button>
+          </div>
+        </Link>
+      </div>
 
+      <div className="ml-10  mb-10" >
+      <h3 className="font-medium text-2xl ">Generate Report</h3>
+      <input type="text"  placeholder="Select period" className="mt-4 rounded-md  "/>
+      <div>
+      <button className="btn py-2 mt-4 ">Download</button>
+      </div>
+      </div>
     </div>
   );
 }
--- src/pages/StudentDashboard.js
+++ src/pages/StudentDashboard.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function StudentDashboard() {
   return (
-    <div className='flex gap-4 p-4'>
-      <Link to='login'>login</Link>
-      <Link to='admin'>admin</Link>
-      <Link to='students'>students</Link>
-      <Link to='teacher'>teacher</Link>
-      <Link to='class'>class</Link>
+    <div className="flex gap-4 p-4">
+      <Link to="login">login</Link>
+      <Link to="admin">admin</Link>
+      <Link to="students">students</Link>
+      <Link to="teacher">teacher</Link>
+      <Link to="class">class</Link>
+      <Link to="dashboardstudent">studentdashboard</Link>
     </div>
-  )
+  );
 }
 
-export default StudentDashboard
+export default StudentDashboard;
