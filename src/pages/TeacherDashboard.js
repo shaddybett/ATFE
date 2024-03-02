6367562d4bedc7b4aaf532c48e583d7ea1465086
Nicholas Njagi <njaginicholas@gmail.com>
2024-03-02 20:12:49 +0300

    update TeacherDashboard.js

--- src/pages/TeacherDashboard.js
+++ src/pages/TeacherDashboard.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Nav from '../components/Nav';
 import grad from '../assets/images/grad.svg';
 import chevRight from '../assets/images/chevron-forward-outline.svg';
@@ -9,16 +9,39 @@ import { Link } from 'react-router-dom';
 import { ListGroup } from 'flowbite-react';
 import CreateClass from './CreateClass';
 import EditClass from './EditClass';
-import { useClassContext } from '../context/ClassContext';
-import { UserContext } from '../context/UserContext';
+import { ClassContext } from '../context/ClassContext';
+import Loading from '../components/Loading';
+import axios from 'axios'
 
 function TeacherDashboard() {
-  const { classes, deleteClass } = useClassContext();
+  const { deleteClass, loading, setLoading, API_URL, setClasses, setError, classes } = useContext(ClassContext) 
   const [hoveredCard, setHoveredCard] = useState(null);
   const [showForm, setShowForm] = useState(false);
   const [showEditClassForm, setShowEditClassForm] = useState(false);
   const [selectedClass, setSelectedClass] = useState(null);
-  const {loading} = useContext(UserContext)
+
+  useEffect(() => {
+    // get all classes
+    setLoading(true)
+    const fetchClasses = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/class`, {
+            headers: {
+                Authorization:`Bearer ${sessionStorage.getItem('authToken')}`
+            }
+        });
+        setClasses(response.data)
+      } catch (error) {
+        setError('Error fetching classes');
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchClasses();
+  }, [API_URL,setClasses, setError, setLoading]);
+
 
   const handleEditClass = (currentClass) => {
     // Open the edit class form and set the selected class
@@ -42,6 +65,7 @@ function TeacherDashboard() {
 
   return (
     <div className={(loading || showForm) ? 'overflow-hidden h-full max-h-[100vh]' :''}>
+        {loading && <Loading />}
       <Nav />
       {showForm && <CreateClass handleClose={() => setShowForm(false)} setShowForm={setShowForm} />}
 
@@ -53,7 +77,7 @@ function TeacherDashboard() {
         />
       )}
 
-      <section className="w-full max-w-7xl mx-auto px-4 py-12 ">
+      <section onClick={()=>setHoveredCard(null)} className="w-full max-w-7xl mx-auto px-4 py-12 ">
         <Link to="/students" className="block w-full max-w-[350px]">
           <div className="flex gap-4 items-center px-4 pb-4 pt-14 rounded-lg bg-light-orange hover:bg-t-orange transition">
             <img className="w-12 h-12" src={grad} alt="icon" />
@@ -72,32 +96,32 @@ function TeacherDashboard() {
           </button>
         </div>
         <div className="flex flex-col gap-6 py-6">
-          {classes.map((classData, index) => (
+          {classes?.map((classData, index) => (
               <div
                     key={classData.id}
-                    className="class-card relative flex justify-between gap-6 items-end w-full max-w-xl bg-light-orange hover:bg-t-orange transition px-4 pt-10 pb-2 rounded-lg"
+                    className="class-card relative flex gap-6 items-end w-full max-w-xl bg-light-orange hover:bg-t-orange transition px-4 pt-10 pb-2 rounded-lg"
                   >
-                    <div>
-                    <Link to={`/class/${classData.id}`}>
+                    <Link className='mr-auto w-full' to={`/class/${classData.id}`}>
+                    <div >
                       <h4 className="font-semibold text-2xl">{classData.class_name}</h4>
                       <p>
                         {classData.start_time} - {classData.end_time}
                       </p>
-                        </Link>
-
                     </div>
+                    </Link>
+
                     <div>
                       <span className="block font-bold text-m-orange">{classData.students.length}</span>
                       <span>Students</span>
                     </div>
                     <button
-                      className="absolute z-50 top-4 right-4 p-1 border border-transparent hover:border-orange-300 rounded-full transition"
+                      className="absolute z-40 top-4 right-4 p-1 border border-transparent hover:border-orange-300 rounded-full transition"
                       aria-label="class options"
                       onClick={() => handleClick(index)}
                     >
                       <img className="w-4 h-4" src={ellipsis} alt="icon" />
                     </button>
-                    <ListGroup className={`list-group ${hoveredCard === index ? 'opacity-100' : 'opacity-0'}`}>
+                    <ListGroup className={`list-group ${hoveredCard === index ? 'opacity-100 scale-100' : 'scale-0'}`}>
                       <ListGroup.Item onClick={() => handleEditClass(classData)}>
                         <img src={pencil} alt="icon" className="w-3 h-3 mr-2" />
                         Edit
